63a8168870aac5f403f7a27bed03b7025e4e12ec
Jose Wills <jose.wills@insurancerevolution.co.uk>
2024-08-09 13:16:42 +0100

    test basic functionality

--- main_test.go
+++ main_test.go
@@ -5,3 +5,26 @@ import "testing"
 func TestCheckoutImplementation(t *testing.T) {
 	var _ ICheckout = (*Checkout)(nil)
 }
+
+func TestCheckout(t *testing.T) {
+	mockRules := map[string]PricingRule{
+		"A": {UnitPrice: 50, DiscountQty: 3, DiscountPrice: 130},
+		"B": {UnitPrice: 30, DiscountQty: 2, DiscountPrice: 45},
+	}
+
+	checkout := NewCheckout(mockRules)
+
+	err := checkout.Scan("A")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	totalPrice, err := checkout.GetTotalPrice()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if totalPrice != 50 {
+		t.Fatalf("expected total price 50, got %d", totalPrice)
+	}
+}
