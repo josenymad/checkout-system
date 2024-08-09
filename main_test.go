561e21b468ddc0fbfd31dd6f627ab347bc24c5cd
Jose Wills <jose.wills@insurancerevolution.co.uk>
2024-08-09 13:56:50 +0100

    test with discount and regular pricing

--- main_test.go
+++ main_test.go
@@ -10,6 +10,8 @@ func TestCheckout(t *testing.T) {
 	mockRules := map[string]PricingRule{
 		"A": {UnitPrice: 50, DiscountQty: 3, DiscountPrice: 130},
 		"B": {UnitPrice: 30, DiscountQty: 2, DiscountPrice: 45},
+		"C": {UnitPrice: 20},
+		"D": {UnitPrice: 15},
 	}
 
 	checkout := NewCheckout(mockRules)
@@ -41,19 +43,50 @@ func TestCheckout(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+
 	err = checkout.Scan("A")
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+
 	err = checkout.Scan("A")
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+
 	totalPrice, err = checkout.GetTotalPrice()
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
+
 	if totalPrice != 130 {
 		t.Fatalf("expected total price 130, got %d", totalPrice)
+	}
+
+	// test with discount and regular pricing
+	checkout = NewCheckout(mockRules)
+
+	err = checkout.Scan("B")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	err = checkout.Scan("C")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	err = checkout.Scan("B")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	totalPrice, err = checkout.GetTotalPrice()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if totalPrice != 65 {
+		t.Fatalf("expected total price 65, got %d", totalPrice)
 	}
 }
